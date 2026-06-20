c55feb1d8a7b414566ae9841094de57922f64325
rakshithkongara03-maker <rakshithkongara03@gmail.com>
2026-06-19 21:57:07 -0500

    requirements.txt added-versions update1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-return render_template("index.html")
+    return render_template("index.html")
 
-if **name** == "**main**":
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
